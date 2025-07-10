a4a5737ac8ac55ab08dcd3003f044ab7ce7f1128
Adham Ehab <adhamehab2112@gmail.com>
2025-07-10 18:49:32 +0200

    Home Page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,20 +1,24 @@
+import HomeMainSection from "../../components/HomeMainSection/HomeMainSection";
 import HomeNavBar from "../../components/HomeNavBar/HomeNavBar";
+import RecentlyViewed from "../../components/RecentlyViewed/RecentlyViewed";
 import SideBar from "../../components/SideBar/SideBar";
 
 function Home() {
   return (
     <>
     <HomeNavBar/>
-    <div className="mt-12 flex flex-col justify-between">
-      <div className="">
+    <div className="mt-12 flex  ">
+      <div className="w-1/5">
         {/* Side Bar */}
         <SideBar/>
       </div>
-      <div className="w-1/4">
+      <div className="w-1/2">
         {/* main */}
+        <HomeMainSection/>
       </div>
-      <div className="">
+      <div className="w-1/3 ml-20">
         {/* recently viewed */}
+        <RecentlyViewed/>
       </div>
       
     </div>
